5f014fed0abb4ca799f462d75b481c5a8d178057
damart24 <82331644+damart24@users.noreply.github.com>
2022-09-21 01:34:28 +0200

    Parallax ya funcional mas bonito el código

--- IndieSpainJam_Project/Assets/Scripts/MoveBackground.cs
+++ IndieSpainJam_Project/Assets/Scripts/MoveBackground.cs
@@ -6,26 +6,23 @@ public class MoveBackground : MonoBehaviour
 {
 	[SerializeField] private float parallaxMultiplier;
 	private Vector2 screenBounds;
-	private Vector3 previousCameraPosition;
-	private float spriteWidth, horizontalMovement, startPosition;
-	private float actualPos;
+	private float spriteWidth, doubleSpriteWidth, speed;
 	void Start()
 	{
 		spriteWidth = GetComponent<SpriteRenderer>().bounds.size.x;
-		horizontalMovement = spriteWidth * 2;
-		startPosition = transform.position.x;
+		speed = 0.01f;
+		doubleSpriteWidth = spriteWidth * 2;
 		screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
 	}
 	void LateUpdate()
 	{
-		float deltax = 0.01f * parallaxMultiplier;
-		actualPos += deltax;
-		transform.Translate(new Vector3(deltax, 0, 0));
+		//Cantidad que se mueve, es decir velocidad
+		float xSpeedMovement = speed * parallaxMultiplier * Time.deltaTime;
+		transform.Translate(new Vector3(xSpeedMovement, 0, 0));
 
-		if (transform.position.x > 19)
-		{
-			transform.position = new Vector3(-19, transform.position.y, transform.position.z);
-			actualPos = -19;
-		}
+		//Si la posicion horizontal es mas grande que la pantalla mas el ancho del sprite
+		//Mueve el objeto al inicio de la pantalla, es decir el ancho de la pantalla - el ancho del sprite por dos
+		if (transform.position.x > screenBounds.x + spriteWidth)
+			transform.position = new Vector3(screenBounds.x - doubleSpriteWidth, transform.position.y, transform.position.z);
 	}
 }
