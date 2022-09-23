2ec823d41b7b2d004c849eecd99730fa33c12113
damart24 <82331644+damart24@users.noreply.github.com>
2022-09-24 00:27:25 +0200

    camerashake funsionaaaaaa

---
+++ IndieSpainJam_Project/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    Vector3 cameraInitialPosition;
+    public float shakeMagnitude = 0.05f, shakeTime = 0.5f;
+    public Camera mainCamera;
+    public void ShakeIt()
+    {
+        cameraInitialPosition = mainCamera.transform.position;
+        InvokeRepeating("StartCameraShaking", 0f, 0.005f);
+        Invoke("StopCameraShaking", shakeTime);
+    }
+    void StartCameraShaking()
+    {
+        float cameraShakingoffsetx = Random.value * shakeMagnitude * 2 - shakeMagnitude;
+        float cameraShakingoffsetY = Random.value * shakeMagnitude * 2 - shakeMagnitude;
+        Vector3 cameraIntermadiatePosition = mainCamera.transform.position;
+        cameraIntermadiatePosition.x += cameraShakingoffsetx;
+        cameraIntermadiatePosition.y += cameraShakingoffsetY;
+        mainCamera.transform.position = cameraIntermadiatePosition;
+    }
+
+    void StopCameraShaking()
+    {
+        CancelInvoke("StartCameraShaking");
+        mainCamera.transform.position = cameraInitialPosition; 
+    }
+    
+}
