5e38ea06c8d2422dca989f6a4fd7b9cae831275a
NachoDelCastillo <nachodelcastillorubio@gmail.com>
2022-09-24 17:05:48 +0200

    wedbwe

--- IndieSpainJam_Project/Assets/Scripts/TutorialManager.cs
+++ IndieSpainJam_Project/Assets/Scripts/TutorialManager.cs
@@ -8,9 +8,12 @@ using TMPro;
 public class TutorialManager : MonoBehaviour
 {
     // Fases del tutorial
-    public enum tutPhases { agarrarCarbonParaTorreta, meterCarbonEnTorreta, 
+    public enum tutPhases
+    {
+        agarrarCarbonParaTorreta, meterCarbonEnTorreta,
         meterseEnTorreta, matarEnemigoTorreta, salirDeTorreta, agarrarCarbonParaMotor, meterCarbonEnMotor,
-        trenEnMarcha}
+        trenEnMarcha
+    }
 
     // El currentPhase muestra lo que el jugador necesita hacer
     tutPhases currentPhase = tutPhases.agarrarCarbonParaTorreta;
@@ -24,16 +27,43 @@ public class TutorialManager : MonoBehaviour
         public SpriteRenderer[] infoImages;
         public TMP_Text[] infoTexts;
     }
-    [SerializeField] tutItems[] TutorialElements;
+    [SerializeField] tutItems[] tutorialElements;
 
 
     private void Awake()
     {
+        HideEverything();
+
         StartCoroutine(DoTutorial());
+    }
+
+    void HideEverything()
+    {
+        foreach (var tutElement in tutorialElements)
+        {
+            Color panelColor = tutElement.panel.color;
+            tutElement.panel.color = new Color(panelColor.r, panelColor.g, panelColor.b, 0);
+
+            // Hide texts
+            foreach (TMP_Text item in tutElement.infoTexts)
+            {
+                Color itemColor = item.color;
+                item.color = new Color(itemColor.r, itemColor.g, itemColor.b, 0);
+            }
+
+            // Hide info Images
+            foreach (SpriteRenderer item in tutElement.infoImages)
+            {
+                Color itemColor = item.color;
+                item.color = new Color(itemColor.r, itemColor.g, itemColor.b, 0);
+            }
+        }
     }
 
     IEnumerator DoTutorial()
     {
+        ShowTutorialItems((tutPhases)0);
+
         yield return new WaitForSeconds(1);
 
         // TUTORIAL DE AGARRAR EL CARBON
@@ -48,17 +78,39 @@ public class TutorialManager : MonoBehaviour
 
     }
 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            for (int i = ((int)tutPhases.trenEnMarcha) - 1; i >= 0; i--)
+                TryToChangePhase((tutPhases)i);
+        }
+    }
 
     // Este metodo se llama cuando se realiza alguna de las acciones necesarias para completar el tutorial
     public void TryToChangePhase(tutPhases phaseDone)
     {
         if (currentPhase == phaseDone)
+        {
+            // Encontrar las referencias de los items de la parte del tutorial actual
+            HideTutorialItems(currentPhase);
+
             currentPhase++;
 
-        // STOP
-        //if (currentPhase = TutPhases.trenEnMarcha)
+            // STOP
+            //if (currentPhase == tutPhases.trenEnMarcha)
+
+            // Si no es el final, mostrar el siguiente
+            ShowTutorialItems(currentPhase);
+        }
     }
 
+    void ShowTutorialItems(tutPhases tutPhase)
+    {
+        // Encontrar las referencias de los items de la parte del tutorial actual
+        tutItems tutItem = Array.Find(tutorialElements, tutItem => tutItem.phase == tutPhase);
+        StartCoroutine(ShowTutorialItems(tutItem));
+    }
     IEnumerator ShowTutorialItems(tutItems phaseItem)
     {
         // Tiempo que tarda en aparecer el panel
@@ -68,17 +120,15 @@ public class TutorialManager : MonoBehaviour
         float infoPanelShowTime = .5f;
 
 
-
-        // Panel
+        // PANEL
 
         // Fade
         SpriteRenderer panel = phaseItem.panel;
-        panel.color = new Color(1, 1, 1, 0);
         panel.DOFade(1, panelShowTime);
 
         // Position
         Vector3 panelPosition = panel.transform.position;
-        phaseItem.panel.transform.position = 
+        phaseItem.panel.transform.position =
             new Vector3(panelPosition.x, panelPosition.y - 1);
         phaseItem.panel.transform.DOMoveY(panelPosition.y, panelShowTime);
 
@@ -90,16 +140,49 @@ public class TutorialManager : MonoBehaviour
         TMP_Text[] infoTexts = phaseItem.infoTexts;
 
         foreach (SpriteRenderer infoImage in infoImages)
-        {
-            infoImage.color = new Color(1, 1, 1, 0);
-            infoImage.DOFade(1, panelShowTime);
-        }
+            infoImage.DOFade(1, infoPanelShowTime);
 
         foreach (TMP_Text infoText in infoTexts)
-        {
-            infoText.color = new Color(1, 1, 1, 0);
-            infoText.DOFade(1, panelShowTime);
-        }
+            infoText.DOFade(1, infoPanelShowTime);
+    }
+
+    void HideTutorialItems(tutPhases tutPhase)
+    {
+        // Encontrar las referencias de los items de la parte del tutorial actual
+        tutItems tutItem = Array.Find(tutorialElements, tutItem => tutItem.phase == tutPhase);
+        StartCoroutine(HideTutorialItems(tutItem));
+    }
+    IEnumerator HideTutorialItems(tutItems phaseItem)
+    {
+        // Tiempo que tarda en aparecer el panel
+        float panelHideTime = 1;
+
+        // Tiempo que tarda en aparecer la informacion del panel (texxtos e imagenes)
+        float infoHideTime = .5f;
+
+
+        // PANEL
+
+        // Fade
+        SpriteRenderer panel = phaseItem.panel;
+        panel.DOFade(0, panelHideTime);
+
+        // Position
+        Vector3 panelPosition = panel.transform.position;
+        phaseItem.panel.transform.DOMoveY(panelPosition.y - 1, panelHideTime);
+
+
+        yield return new WaitForSeconds(panelHideTime);
+
+        // Info in the Panel
+        SpriteRenderer[] infoImages = phaseItem.infoImages;
+        TMP_Text[] infoTexts = phaseItem.infoTexts;
+
+        foreach (SpriteRenderer infoImage in infoImages)
+            infoImage.DOFade(0, infoHideTime);
+
+        foreach (TMP_Text infoText in infoTexts)
+            infoText.DOFade(0, infoHideTime);
     }
 
 
