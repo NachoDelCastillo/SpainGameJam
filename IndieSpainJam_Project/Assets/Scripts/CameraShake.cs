c2cebe29ac52ac3addc4428a6d460a5a2e4c7f43
damart24 <82331644+damart24@users.noreply.github.com>
2022-09-24 16:12:49 +0200

    CameraShakeFunciona

--- IndieSpainJam_Project/Assets/Scripts/CameraShake.cs
+++ IndieSpainJam_Project/Assets/Scripts/CameraShake.cs
@@ -7,9 +7,29 @@ public class CameraShake : MonoBehaviour
     Vector3 cameraInitialPosition;
     public float shakeMagnitude = 0.05f, shakeTime = 0.5f;
     public Camera mainCamera;
-    public void ShakeIt()
+    bool cameraReturningOriginalPos = false;
+    private void Start()
     {
         cameraInitialPosition = mainCamera.transform.position;
+    }
+    private void Update()
+    {
+        if (Vector3.Distance(mainCamera.transform.localPosition, cameraInitialPosition) <= 0.1f)
+        {
+            cameraReturningOriginalPos = false;
+            mainCamera.transform.position = cameraInitialPosition;
+        }
+        if (cameraReturningOriginalPos)
+        {
+            mainCamera.transform.localPosition = Vector3.Lerp(mainCamera.transform.localPosition, cameraInitialPosition, 10 * Time.deltaTime);
+            Debug.Log(mainCamera.transform.localPosition);
+            Debug.Log(mainCamera.transform.position);
+            //Debug.Log(mainCamera.transform.localPosition == cameraInitialPosition);
+        }
+    }
+    public void ShakeIt()
+    {
+        cameraReturningOriginalPos = false;
         InvokeRepeating("StartCameraShaking", 0f, 0.005f);
         Invoke("StopCameraShaking", shakeTime);
     }
@@ -20,14 +40,13 @@ public class CameraShake : MonoBehaviour
         Vector3 cameraIntermadiatePosition = mainCamera.transform.position;
         cameraIntermadiatePosition.x += cameraShakingoffsetx;
         cameraIntermadiatePosition.y += cameraShakingoffsetY;
-        mainCamera.transform.position = cameraIntermadiatePosition;
+        mainCamera.transform.localPosition = cameraIntermadiatePosition;
     }
 
     void StopCameraShaking()
     {
         CancelInvoke("StartCameraShaking");
-        mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, cameraInitialPosition, 0.5f * Time.deltaTime);
-        if(mainCamera.transform.position != cameraInitialPosition) Invoke("StopCameraShaking", shakeTime);
+        cameraReturningOriginalPos = true;
     }
     
 }
